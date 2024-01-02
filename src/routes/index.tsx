3525953eeb85d523ad141ee5c280df6ce0c784ac
Matheus Viana <67674592+Vianahdev@users.noreply.github.com>
2024-01-02 15:16:05 -0300

    Implantação do Material UI

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,10 @@
+import { Button } from "@mui/material";
 import { Routes, Route , Navigate} from "react-router-dom"
 
 export const AppRoutes = () =>{
     return(
         <Routes>
-            <Route path="/pagina-inicial" element={<p>Página Inicial</p>}/>
+            <Route path="/pagina-inicial" element={<Button>Teste</Button>}/>
 
             <Route path="*" element={<><h1>Erro 404</h1> <p>Página não Encontrada</p></>} />
         </Routes>
